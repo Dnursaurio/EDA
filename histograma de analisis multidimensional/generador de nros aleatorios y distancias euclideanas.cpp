53c1977bf7ee9431a4f2ff760722ee4ec6269f2d
Alumno B305 <alumno-b305@UCSP.AULAB>
2026-08-27 16:36:42 -0500

    distancias euclideanas multiples

---
+++ histograma de analisis multidimensional/generador de nros aleatorios y distancias euclideanas.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+int dist_euclidiana(vector<double>va1,vector<doubler>va2)
+{
+	double distancia = 0;
+	vector<double>::iterator i = va1.begin();
+	vector<double>::iterator j = va2.begin();
+	double suma = 0;
+	for(;i!=va1.end() && j!=va2.end();i++)
+	{
+		double temp = pow(*i - *j, 2);
+		suma += temp;
+		j++;
+	}
+	distancia = sqrt(suma);
+	cout<<distancia;
+	return distancia;
+}
+
+int main()
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dis(0.0,1.0);
+	int dim = 0;
+	cout<<"ingrese el numero de dimensiones: ";
+	cin>>dim;
+	vector<vector<double>>mapa;
+	for(int i = 0; i < 100; i++)
+	{
+		vector<double> nros;
+		for(int j = 0; j < dim;j++)
+		{
+			nros.push_back(dis(gen));
+		}
+		mapa.push_back(nros);
+	}
+	vector<vector<double>>::iterator i = mapa.begin();
+	vector<vector<double>>::iterator j = mapa[i];
+	for(;i<mapa.end();i++)
+	{
+		
+	}
+}
