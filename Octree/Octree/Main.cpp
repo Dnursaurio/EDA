dca4bb982cd4dd6b84724e35f2f34c29d15bfb72
Alumno B305 <alumno-b305@UCSP.AULAB>
2026-09-17 18:17:29 -0500

    octree

---
+++ Octree/Octree/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "		Octree		" << endl;
+	return 0;
+}
---
+++ Octree/Octree/Octree.hpp
@@ -0,0 +1,28 @@
+#ifndef OCTREE_H
+#define OCTREE_H
+
+//Un punto tridimencional XYZ
+struct Punto
+{
+	int x;
+	int y;
+	int z;
+
+	Punto(int a, int b, int c)
+	{
+		x = a;
+		y = b;
+		z = c;
+	}
+};
+
+
+// El Octree
+class Octree
+{
+public:
+	//metodos
+private:
+	//variables
+};
+#endif
